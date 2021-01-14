877389e2c160102e0876eec664cdcb2b984fef5a
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2021-01-13 21:09:51 -0600

    feat(context): added context files for theming and general application context that may be needed

---
+++ src/context/themeContext.js
@@ -0,0 +1,16 @@
+export default themeContext = {
+  blackDark: '#000000',
+  black: '#141414',
+  blackLight: '#292929',
+
+  white: '#fffffe',
+  whiteDark: '#F5F5F5',
+
+  greyLight: '#E0E0E0',
+  grey: '#C4C9CB',
+  greyDark: '#A9B0B2',
+
+  baseColor: '#DDBB55',
+
+  accentColor: '#247B7B',
+};
