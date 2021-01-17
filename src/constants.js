0792c12d349505d3d78b73ba0e5812a0be0adf3a
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2021-01-17 09:24:32 -0600

    chore: Added constants.js file to house abstracted values that do not change for now to make them easy to change later. These values cannot go in .env file because they are slightly more eviro

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-const { REACT_OMBD_API_KEY: OMBD_KEY } = process.env;
+const { REACT_APP_OMBD_API_KEY: OMBD_KEY } = process.env;
 
-export const OMBD_REQUEST_URL = `http://www.omdbapi.com/?apikey=${OMBD_KEY}`;
+export const OMBD_REQUEST_URL = `http://www.omdbapi.com/?apikey=${OMBD_KEY}&type=movie`;
