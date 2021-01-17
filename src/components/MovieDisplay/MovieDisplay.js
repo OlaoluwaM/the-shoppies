fc7a976368b74af0c5618ad71d38b7ea77c6702d
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2021-01-17 11:49:00 -0600

    refactor: Abstracted movie poster logic to its own component and animated it

--- src/components/MovieDisplay/MovieDisplay.js
+++ src/components/MovieDisplay/MovieDisplay.js
@@ -1,19 +1,42 @@
 import PropTypes from 'prop-types';
 
-import { m as motion } from 'framer-motion';
 import { default as NoImage } from '../../reusables/NoImage';
 import { MovieDisplayContainer, MovieInfoWrapper } from './MovieDisplayStyles';
+import { m as motion, AnimatePresence, AnimateSharedLayout } from 'framer-motion';
 
+function MoviePoster({ imageSrc, movieTitle }) {
+  return (
+    <AnimatePresence>
+      {imageSrc ? (
+        <motion.img
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          initial={{ opacity: 0 }}
+          src={imageSrc}
+          alt={movieTitle}
+          layout
+          key="image"
+        />
+      ) : (
+        <NoImage
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          initial={{ opacity: 0 }}
+          className="default-poster"
+          layout
+          key="poster"
+          title="Poster not available"
+        />
+      )}
+    </AnimatePresence>
+  );
+}
 export default function MovieDisplay({ poster, title, year }) {
   const imageSrc = poster.search('N/A') > -1 ? false : poster;
 
   return (
     <MovieDisplayContainer layout whileHover="hover">
-      {imageSrc ? (
-        <motion.img src={imageSrc} alt={title} layout key="image" />
-      ) : (
-        <NoImage className="default-poster" layout key="poster" />
-      )}
+      <MoviePoster imageSrc={imageSrc} movieTitle={title} />
 
       <MovieInfoWrapper layout="position">
         <h4>{title}</h4>
@@ -27,4 +50,9 @@ MovieDisplay.propTypes = {
   poster: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   year: PropTypes.number.isRequired,
+};
+
+MoviePoster.propTypes = {
+  imageSrc: PropTypes.string,
+  movieTitle: PropTypes.string.isRequired,
 };
