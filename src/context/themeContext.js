106db469bf6464d79a786a3caad1984ae09258f1
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2021-01-17 09:32:02 -0600

    feat: Created file to house application theme palette to be used with styled-components package for themeing

--- src/context/themeContext.js
+++ src/context/themeContext.js
@@ -1,18 +1,20 @@
 const themeContext = {
-  blackDark: '#000000',
-  black: '#141414',
-  blackLight: '#292929',
+  dark: {
+    blackDark: '#000000',
+    black: '#141414',
+    blackLight: '#292929',
 
-  white: '#fffffe',
-  whiteDark: '#F5F5F5',
+    white: '#ffffff',
+    whiteDark: '#F5F5F5',
 
-  greyLight: '#E0E0E0',
-  grey: '#C4C9CB',
-  greyDark: '#A9B0B2',
+    greyLight: '#E0E0E0',
+    grey: '#C4C9CB',
+    greyDark: '#A9B0B2',
 
-  baseColor: '#DDBB55',
+    baseColor: '#FFD61F',
 
-  accentColor: '#247B7B',
+    accentColor: '#3C787E',
+  },
 };
 
 export default themeContext;
