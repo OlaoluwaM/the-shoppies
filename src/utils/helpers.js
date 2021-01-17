f505f381325c9528ea9f57ef117a1638f1db9d50
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2021-01-17 09:32:50 -0600

    feat: Created helpers file to house helper functions

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,39 @@
 import { OMBD_REQUEST_URL } from '../constants';
 
-export function generateRequestUrl(queryString = '') {
-  queryString = queryString.startsWith('&', 0) ? queryString : `&${queryString}`;
-  return `${OMBD_REQUEST_URL}${queryString}`;
+export function generateRequestUrlObject(queryStringArray = []) {
+  const reqUrl = new URL(OMBD_REQUEST_URL);
+
+  const containsOnlyArrays = queryStringArray.every(itm => Array.isArray(itm));
+
+  if (queryStringArray.length > 0 && containsOnlyArrays) {
+    queryStringArray.forEach(([key, value]) => {
+      if (!(key && value)) return;
+
+      if (reqUrl.searchParams.has(key)) {
+        reqUrl.searchParams.set(key, value);
+      } else reqUrl.searchParams.append(key, value);
+    });
+  }
+
+  return reqUrl;
+}
+
+export function hasScrolledToBottom(element = document) {
+  return window.innerHeight + window.scrollY >= element.offsetHeight;
+}
+
+export function debounce(callback, delayTime) {
+  let timeout;
+
+  return (...args) => {
+    const toBeRanLater = () => {
+      timeout = null;
+
+      callback(...args);
+    };
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(toBeRanLater, delayTime);
+  };
 }
