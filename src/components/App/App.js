142399555928ddcc883a8a65c8bf319898f276b5
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2021-01-17 19:01:31 -0600

    feat: added welcome messages and nomination route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,33 +3,69 @@ import Loading from '../../reusables/Loading';
 import themeContext from '../../context/themeContext';
 
 import { ThemeProvider } from 'styled-components';
-import { Suspense, lazy, useState } from 'react';
+import { getNominationList } from '../../utils/helpers';
+import { default as toast, Toaster } from 'react-hot-toast';
 import { Route, Switch, useLocation } from 'react-router-dom';
+import { Suspense, lazy, useState, useEffect, useRef } from 'react';
 import {
   ExitFeature,
   MotionConfig,
-  AnimatePresence,
   GesturesFeature,
   AnimationFeature,
   AnimateLayoutFeature,
 } from 'framer-motion';
 
 const MoviesSearch = lazy(() => import('../MoviesView/MoviesView'));
+const Nominations = lazy(() => import('../NominationView/NominationView'));
 
-function App() {
+export default function App() {
   const location = useLocation();
   const [applicationTheme, setTheme] = useState('dark');
+  const timeoutRef = useRef([]);
+
+  useEffect(() => {
+    if (getNominationList().size > 0) return;
+
+    const welcomeMessages = [
+      'Hello 👋! Welcome to the shoppies',
+      'Here you can search for movies and nominate your favorite ones for a shoppie award',
+      'Keep in mind however that you can only nominate five movies',
+      'That is all the necessary information, happy nominating!',
+    ];
+
+    welcomeMessages.forEach((message, ind) => {
+      let timeout = setTimeout(() => {
+        toast(message, { duration: 4000 });
+      }, ind * 5000);
+
+      timeoutRef.current.push(timeout);
+    });
+
+    return () => timeoutRef.current.forEach(timeout => clearTimeout(timeout));
+  }, []);
 
   return (
     <ThemeProvider theme={themeContext[applicationTheme]}>
       <MotionConfig
         features={[AnimateLayoutFeature, AnimationFeature, ExitFeature, GesturesFeature]}>
-        <NavBar />
+        <Toaster
+          position="top-right"
+          reverseOrder={false}
+          toastOptions={{
+            className: 'toast',
+          }}
+        />
+
+        <NavBar setTheme={setTheme} />
         <>
           <Suspense fallback={<Loading fullscreen={true} />}>
             <Switch location={location} key={location.pathname}>
               <Route exact path="/">
                 <MoviesSearch />
+              </Route>
+
+              <Route path="/nominations">
+                <Nominations />
               </Route>
             </Switch>
           </Suspense>
@@ -38,5 +74,3 @@ function App() {
     </ThemeProvider>
   );
 }
-
-export default App;
