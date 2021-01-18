c58d350007d8a88b7194b3c2f004b92b1dd8b03a
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2021-01-17 19:19:27 -0600

    feat: Added light theme to palette

--- src/context/themeContext.js
+++ src/context/themeContext.js
@@ -4,14 +4,33 @@ const themeContext = {
     black: '#141414',
     blackLight: '#292929',
 
-    white: '#ffffff',
+    whiteLight: '#ffffff',
+    white: '#EBEBEB',
     whiteDark: '#F5F5F5',
 
     greyLight: '#E0E0E0',
     grey: '#C4C9CB',
     greyDark: '#A9B0B2',
 
-    baseColor: '#3C787E',
+    baseColor: '#197278',
+    error: '#ff0033',
+  },
+
+  light: {
+    blackDark: '#F5F5F5',
+    blackLight: '#ffffff',
+    black: '#EBEBEB',
+
+    whiteDark: '#000000',
+    white: '#141414',
+    whiteLight: '#292929',
+
+    greyLight: '#E0E0E0',
+    grey: '#C4C9CB',
+    greyDark: '#A9B0B2',
+
+    baseColor: '#197278',
+    error: '#ff0033',
   },
 };
 
