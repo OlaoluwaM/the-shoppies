d93ba8cb67154602affb5eb7a9e3e251c9abf65f
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2021-01-17 19:15:15 -0600

    feat: Added constant values representing nomination list limit and localStorage key value for easy changing in the future

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,7 @@
 const { REACT_APP_OMBD_API_KEY: OMBD_KEY } = process.env;
 
 export const OMBD_REQUEST_URL = `http://www.omdbapi.com/?apikey=${OMBD_KEY}&type=movie`;
+
+export const STORED_NOMINATIONS = 'storedNominations';
+
+export const NOMINEE_LIMIT = 5;
